0ee692ad7d9c625b4922084a5dc8de1bb5498675
Derek <derekdanielson84@gmail.com>
2023-03-15 13:09:41 -0600

    Built-in Constructor notes

--- app.js
+++ app.js
@@ -1,17 +1,90 @@
-// Person Constructor
-function Person(name, dob){
-    this.name = name;
-    //this.age = age;
-    this.birthday = new Date(dob);
-    this.calculateAge = function(){
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+// Strings
+const name1 = 'Bob';
+const name2 = new String('Bob');
+
+console.log(name1);
+console.log(name2);  // Object
+
+console.log(typeof name1); //String
+console.log(typeof name2); //Object
+
+if(name1 === 'Bob'){
+    console.log('yes');
+} else {
+    console.log('no');
+}                           //Yes
+
+
+if(name2 === 'Bob'){
+    console.log('yes');
+} else {
+    console.log('no');
+}                           //No
+
+
+// Number
+const num1 = 3;
+const num2 = new Number(3); //Object
+
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true); //Object
+
+// Function
+const getSum1 = function(x, y){
+    return x + y;
 }
 
-//const brad = new Person('Brad', 25);
-//const bob = new Person('Bob', 38);
+console.log(getSum1(1, 1)); // 2
 
-const bob = new Person('Bob', '9-10-1981');
-console.log(bob.calculateAge());
+const getSum2 = new Function('x', 'y', 'return 1+1');
+
+// Object
+const bob = {name: 'Bob'};
+const bob2 = new Object({name: 'Bob'});
+
+// Arrays
+const array1 = [1,2,3,4];
+const array2 = new Array(1,2,3,4);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
